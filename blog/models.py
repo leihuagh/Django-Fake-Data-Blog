49efd8079261233798f3a39a6617ea671cec029b
Lei Hua <lhua@example.com>
2019-03-16 09:10:34 -0400

    home slides

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from core.models import TimeStampModel
 
-# Create your models here.
+
+class Post(TimeStampModel):
+    pass
