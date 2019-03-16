20feb410aa447798c669fa996021010b909b2002
Lei Hua <lhua@example.com>
2019-03-15 23:21:33 -0400

    basic register

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import index, about, contact
+from authentication.views import login_view
+from accounts.views import register
 
 urlpatterns = [
     path('', index, name='index'),
     path('about', about, name='about'),
     path('contact', contact, name='contact'),
+    path('login', login_view, name='login'),
+    path('register', register, name='register'),
 ]
