cc7e69f73e5a395d50fac6b76857137fd3fdcce5
Lei Hua <lhua@example.com>
2019-03-16 00:05:34 -0400

    login

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm
 
 
 def login_view(request):
-    return render(request, 'login.html', {})
+    form = LoginForm(request.POST or None)
+    context = {
+        'form': form,
+        "alert": "none"
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            form = LoginForm()
+            context["alert"] = "block"
+
+    return render(request, 'login.html', context)
 
 
 def logout_view(request):
