cf0cac387a7ba7670934ac7be09ea664983b9060
Lei Hua <lhua@example.com>
2019-03-24 17:20:04 -0400

    fixed time of day templatetag

--- core/templatetags/datetime_tags.py
+++ core/templatetags/datetime_tags.py
@@ -1,19 +1,17 @@
-import datetime
-import pytz
+
 from django.conf import settings
 from django import template
+import datetime
 
 register = template.Library()
 
 
 @register.simple_tag
 def time_of_day():
-    cur_time = datetime.datetime.now(
-        tz=pytz.timezone(str(settings.TIME_ZONE)))
-    print(cur_time.hour)
-    if cur_time.hour < 12:
+    hour = datetime.datetime.now().hour
+    if hour < 12:
         return 'Morning'
-    elif 12 <= cur_time.hour < 18:
+    elif 12 <= hour < 18:
         return 'Afternoon'
     else:
         return 'Evening'
