2ec18548845d0763007bfed70d20586a9b984381
Lei Hua <lhua@example.com>
2019-03-24 21:25:28 -0400

    register

---
+++ accounts/forms.py
@@ -0,0 +1,81 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control mb-3",
+                "placeholder": "Username"
+            }
+        )
+    )
+
+    first_name = forms.CharField(
+        label='First Name',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control mb-3",
+                "placeholder": "First Name"
+            }
+        )
+    )
+
+    last_name = forms.CharField(
+        label='Last Name',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control mb-3",
+                "placeholder": "Last Name"
+            }
+        )
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control mb-3",
+                "placeholder": "Your email"
+            }
+        )
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control mb=3"
+            }
+        )
+    )
+
+    password2 = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control mb-3"
+            }
+        )
+    )
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        qs = User.objects.filter(username=username)
+        if qs.exists():
+            raise forms.ValidationError("Username is taken")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            raise forms.ValidationError("email is taken")
+        return email
+
+    def clean(self):
+        data = self.cleaned_data
+        password = self.cleaned_data.get('password')
+        password2 = self.cleaned_data.get('password2')
+        if password2 != password:
+            raise forms.ValidationError("Passwords must match.")
+        return data
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .forms import RegisterForm
 
 
 def register(request):
-    return render(request, 'register.html', {})
+    form = RegisterForm(request.POST or None)
+    context = {
+        "form": form,
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        first_name = form.cleaned_data.get('first_name')
+        last_name = form.cleaned_data.get('last_name')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        user = User.objects.create_user(username, email, password)
+        if user is not None:
+            user.first_name = first_name
+            user.last_name = last_name
+            user.save()
+            return redirect('/login')
+        else:
+            form = RegisterForm()
+
+    return render(request, 'register.html', context)
