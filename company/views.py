e59a14611a46da874e8f57f211b0c1266242b9dc
Lei Hua <lhua@example.com>
2019-03-25 20:48:17 -0400

    contact form

--- company/views.py
+++ company/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 
 
 def about(request):
@@ -6,4 +7,20 @@ def about(request):
 
 
 def contact(request):
-    return render(request, 'contact.html', {})
+    form = ContactForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        print(form.cleaned_data.get('fullname'))
+        print(form.cleaned_data.get('email'))
+        print(form.cleaned_data.get('content'))
+        form = ContactForm()
+        return redirect('/contact')
+    # if request.method == 'POST':
+    #     print(request.POST)
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
+    return render(request, 'contact.html', context)
